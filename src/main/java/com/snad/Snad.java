956a721cd2e0ee0e3301653b2e5d0ef3dddd2a37
ziro <palembani@gmail.com>
2020-12-17 13:14:36 +0700

    Yo... snad go brrrrr

--- src/main/java/com/snad/Snad.java
+++ src/main/java/com/snad/Snad.java
@@ -1,13 +1,25 @@
 package com.snad;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
+import net.minecraft.block.FallingBlock;
+import net.minecraft.block.Material;
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
 
 public class Snad implements ModInitializer
 {
 
+	public static final FallingBlock SNAD = new FallingBlock(FabricBlockSettings.of(Material.SOLID_ORGANIC).hardness(0.5f));
+	
 	@Override
 	public void onInitialize()
 	{
+		Registry.register(Registry.BLOCK, new Identifier("snad", "snad"), SNAD);
+		Registry.register(Registry.ITEM, new Identifier("snad", "snad"), new BlockItem(SNAD, new Item.Settings().group(ItemGroup.BUILDING_BLOCKS)));
 		System.out.println("Hello Fabric world!");
 	}
 
