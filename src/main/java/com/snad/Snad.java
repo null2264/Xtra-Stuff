ec72eb13e4edec25a9264fb09dd9545e6543bce0
ziro <palembani@gmail.com>
2020-12-17 12:22:41 +0700

    Rename

---
+++ src/main/java/com/snad/Snad.java
@@ -0,0 +1,14 @@
+package com.snad;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Snad implements ModInitializer
+{
+
+	@Override
+	public void onInitialize()
+	{
+		System.out.println("Hello Fabric world!");
+	}
+
+}
