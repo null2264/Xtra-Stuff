6834fe1ee716f5835f3c3816ef93616c5ce579bd
ziro <palembani@gmail.com>
2020-12-18 09:51:12 +0700

    Inject instead of Overwrite

--- src/main/java/com/snad/mixin/SugarCaneMixin.java
+++ src/main/java/com/snad/mixin/SugarCaneMixin.java
@@ -6,6 +6,9 @@ import com.snad.registry.ModTags;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
@@ -24,27 +27,10 @@ public class SugarCaneMixin extends Block {
         super(settings);
     }
     
-    @Overwrite
-    public boolean canPlaceAt(BlockState state, WorldView world, BlockPos pos) {
-        BlockState blockState = world.getBlockState(pos.down());
-        if (blockState.getBlock() == this) {
-            return true;
-        } else {
-            // TODO: Do not overwrite but modify the if statements
-            if (blockState.isOf(Blocks.GRASS_BLOCK) || blockState.isOf(Blocks.DIRT) || blockState.isOf(Blocks.COARSE_DIRT) || blockState.isOf(Blocks.PODZOL) || blockState.isIn(BlockTags.SAND) || blockState.isIn(ModTags.SNAD)) {
-                BlockPos blockPos = pos.down();
-                Iterator<Direction> var6 = Direction.Type.HORIZONTAL.iterator();
-
-                while(var6.hasNext()) {
-                    Direction direction = (Direction)var6.next();
-                    BlockState blockState2 = world.getBlockState(blockPos.offset(direction));
-                    FluidState fluidState = world.getFluidState(blockPos.offset(direction));
-                    if (fluidState.isIn(FluidTags.WATER) || blockState2.isOf(Blocks.FROSTED_ICE)) {
-                        return true;
-                    }
-                }
-            }
-            return false;
-        }
+    @Inject(method = "canPlaceAt(Lnet/minecraft/block/BlockState;Lnet/minecraft/world/WorldView;Lnet/minecraft/util/math/BlockPos;)Z", at = @At("RETURN"), cancellable = true)
+    public void canPlaceAt(BlockState state, WorldView world, BlockPos pos, CallbackInfoReturnable<Boolean> cir)
+    {
+        BlockState blockState2 = world.getBlockState(pos.down());
+        cir.setReturnValue(blockState2.isIn(ModTags.SNAD) || cir.getReturnValue());
     }
 }
